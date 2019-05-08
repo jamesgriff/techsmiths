322719a02a9be1d7e06d3ac7aa64ae7aec16e37f
James Griffiths <james.griffiths@softwire.com>
2019-05-08 12:03:03 +0100

    "Contact Us" section at the end of each page

---
+++ main.js
@@ -0,0 +1,49 @@
+
+let contactUsFormSubmitting = false;
+
+function submitContactUsForm() {
+    if (contactUsFormSubmitting) {
+        return true;
+    }
+    contactUsFormSubmitting = true;
+
+    let body = {
+        name : document.getElementById('contact-us-form-name').value,
+        email : document.getElementById('contact-us-form-email').value,
+        message : document.getElementById('contact-us-form-message').value
+    };
+
+    setContactUsFormStatus('sending');
+    post('https://rd36a554m2.execute-api.eu-west-1.amazonaws.com/default/techsmiths-contact-form',
+        body,
+        () => { // success
+            setContactUsFormStatus('success');
+            contactUsFormSubmitting = false;
+        },
+        () => { // error
+            setContactUsFormStatus('error');
+            contactUsFormSubmitting = false;
+        }
+    )
+}
+
+function post(url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                success(xhr.responseText);
+            } else {
+                error();
+            }
+        }
+    };
+    xhr.send(JSON.stringify(data));
+}
+
+function setContactUsFormStatus(status) {
+    document.getElementById('contact-us-form').classList.remove('sending', 'success', 'error');
+    document.getElementById('contact-us-form').classList.add(status);
+}
