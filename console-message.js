e8a8a4883440a0cf33888bf41e2733c3cf28d201
James Griffiths <james.griffiths@softwire.com>
2019-06-04 15:51:13 +0100

    Add pretty console.log message pointing at the GitHub repo

---
+++ console-message.js
@@ -0,0 +1,19 @@
+
+console.log('%cHi there! 👋\n' +
+            '\n' +
+            '%cLooks like you\'re interested in how we made this site?\n' +
+            '\n' +
+            'We\'ve made all the code available over on our GitHub account 😊\n' +
+            // 'Have a look:\n' +
+            'https://bit.ly/techsmiths-on-gh\n' +
+            '\n' +
+            'What do you like about it?\n' +
+            'What do you hate about it?\n' +
+            'Have you found any bugs?\n' +
+            '\n' +
+            'We\'d love to hear!\n' +
+            'Get in touch:\n' +
+            'https://www.techsmiths.uk/contact\n',
+            
+            'color: #ffc100; font-size: 36px;',
+            'color: #ffc100; font-size: 20px;');
